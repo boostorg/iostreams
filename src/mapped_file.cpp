7230ceafa2129670037f3964f0c9b703d8e28977
Jonathan Turkanis <j@coderage.com>
2005-10-07 17:12:33 +0000

    fix for read-only files
    [SVN r31224]

--- src/mapped_file.cpp
+++ src/mapped_file.cpp
@@ -181,7 +181,9 @@ void mapped_file_source::open_impl(mapped_file_params p)
                        (p.new_file_size != 0 && !readonly) ? 
                            CREATE_ALWAYS : 
                            OPEN_EXISTING,
-                       FILE_ATTRIBUTE_TEMPORARY,
+                       readonly ?
+                           FILE_ATTRIBUTE_READONLY :
+                           FILE_ATTRIBUTE_TEMPORARY,
                        NULL );
 
     if (pimpl_->handle_ == INVALID_HANDLE_VALUE)
