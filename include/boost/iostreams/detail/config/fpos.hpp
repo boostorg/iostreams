7c2592c770975909bee33726f76744321a46c439
Edward Diener <eldiener@tropicsoft.com>
2016-09-16 18:29:04 -0400

    Don't use _FPOSOFF on VC++14 on up.

--- include/boost/iostreams/detail/config/fpos.hpp
+++ include/boost/iostreams/detail/config/fpos.hpp
@@ -22,10 +22,19 @@
 # pragma once
 #endif
 
+#include <boost/config.hpp>
+
 # if (defined(_YVALS) || defined(_CPPLIB_VER)) && !defined(__SGI_STL_PORT) && \
      !defined(_STLPORT_VERSION) && !defined(__QNX__)
      /**/
 #  define BOOST_IOSTREAMS_HAS_DINKUMWARE_FPOS
+
+#if defined(BOOST_MSVC) && BOOST_MSVC >= 1900
+#define BOOST_IOSTREAMS_FPOSOFF(fp) ((long long)(fp))
+#else
+#define BOOST_IOSTREAMS_FPOSOFF(fp) _FPOSOFF(fp)
+#endif
+
 # endif
 
 #endif // #ifndef BOOST_IOSTREAMS_DETAIL_CONFIG_FPOS_HPP_INCLUDED
