dbfdee75159cbcc06f3e389069bac22637f7dc2f
Reimar Döffinger <reimar.doffinger@arm.com>
2017-07-26 10:42:52 +0200

    Clarify origin and authors of recently added files.
    As git history isn't useful in this case, it might be good
    to add more details in the header instead.

--- src/lzma.cpp
+++ src/lzma.cpp
@@ -1,3 +1,6 @@
+// (C) Copyright Milan Svoboda 2008.
+// Originally developed under the fusecompress project.
+// Based on bzip2.cpp by:
 // (C) Copyright Jonathan Turkanis 2003.
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.)
