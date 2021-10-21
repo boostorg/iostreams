80608fe0e9edd7b0618408b2b56617cc925e5968
Jay Krell <jay.krell@cornell.edu>
2021-10-20 19:21:18 -0700

    Change noexcept to BOOST_NOEXCEPT; remove override.

--- include/boost/iostreams/filtering_stream.hpp
+++ include/boost/iostreams/filtering_stream.hpp
@@ -93,7 +93,7 @@ protected:
                  typename Chain::char_type, 
                  typename Chain::traits_type
             >::stream_type                                stream_type;
-    ~filtering_stream_base() noexcept override = default;
+    ~filtering_stream_base() BOOST_NOEXCEPT = default;
     filtering_stream_base() : stream_type(0) { this->set_chain(&chain_); }
 private:
     void notify() { this->rdbuf(chain_.empty() ? 0 : &chain_.front()); }
